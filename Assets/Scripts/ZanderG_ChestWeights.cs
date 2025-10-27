2c52f0b00916ebf23197fdf2cc752b95d7386ee8
Zander Goodwin <zanderngoodwin@gmail.com>
2025-10-27 11:06:14 -0300

    Added script and empty object for script

--- Assets/Scripts/ZanderG_ChestWeights.cs
+++ Assets/Scripts/ZanderG_ChestWeights.cs
@@ -2,6 +2,20 @@ using UnityEngine;
 
 public class ZanderG_ChestWeights : MonoBehaviour
 {
+    [Header("Chest Types")]
+    [SerializeField] int WoodChest;
+    [SerializeField] int BronzeChest;
+    [SerializeField] int SilverChest;
+    [SerializeField] int GoldChest;
+    [SerializeField] int PlatinumChest;
+
+    [Header("Wooden Chest")]
+    [SerializeField] int WoodChestCommon;
+    [SerializeField] int WoodChestUncommon;
+    [SerializeField] int WoodChestRare;
+    [SerializeField] int WoodChestEpic;
+    [SerializeField] int WoodChestLegendary;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
