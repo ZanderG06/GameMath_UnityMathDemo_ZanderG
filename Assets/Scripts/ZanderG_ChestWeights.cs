7549d5d8ca6635a2aa7a67a3c73c2048cd322f21
Zander Goodwin <zanderngoodwin@gmail.com>
2025-10-27 11:13:53 -0300

    Added weights

--- Assets/Scripts/ZanderG_ChestWeights.cs
+++ Assets/Scripts/ZanderG_ChestWeights.cs
@@ -16,6 +16,34 @@ public class ZanderG_ChestWeights : MonoBehaviour
     [SerializeField] int WoodChestEpic;
     [SerializeField] int WoodChestLegendary;
 
+    [Header("Bronze Chest")]
+    [SerializeField] int BronzeChestCommon;
+    [SerializeField] int BronzeChestUncommon;
+    [SerializeField] int BronzeChestRare;
+    [SerializeField] int BronzeChestEpic;
+    [SerializeField] int BronzeChestLegendary;
+
+    [Header("Silver Chest")]
+    [SerializeField] int SilverChestCommon;
+    [SerializeField] int SilverChestUncommon;
+    [SerializeField] int SilverChestRare;
+    [SerializeField] int SilverChestEpic;
+    [SerializeField] int SilverChestLegendary;
+
+    [Header("Gold Chest")]
+    [SerializeField] int GoldChestCommon;
+    [SerializeField] int GoldChestUncommon;
+    [SerializeField] int GoldChestRare;
+    [SerializeField] int GoldChestEpic;
+    [SerializeField] int GoldChestLegendary;
+
+    [Header("Platinum Chest")]
+    [SerializeField] int PlatinumChestCommon;
+    [SerializeField] int PlatinumChestUncommon;
+    [SerializeField] int PlatinumChestRare;
+    [SerializeField] int PlatinumChestEpic;
+    [SerializeField] int PlatinumChestLegendary;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
